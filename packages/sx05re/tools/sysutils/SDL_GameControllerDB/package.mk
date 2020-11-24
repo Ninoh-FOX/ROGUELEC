5d822f0e7664c339f2e55b4e401d6a5a13aa5812
Ninoh-FOX <ninohfox@gmail.com>
2020-11-24 16:52:43 +0100

    add openbor patch beta1

--- packages/sx05re/tools/sysutils/SDL_GameControllerDB/package.mk
+++ packages/sx05re/tools/sysutils/SDL_GameControllerDB/package.mk
@@ -2,11 +2,10 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="SDL_GameControllerDB"
-PKG_VERSION="c5992e0edc141be1fd0764c810792b4ead26e25c"
-PKG_SHA256="8e86c7b05d442b114f150d96958d7e310b28b9bb0b802bc54d0fad26ddcbc2af"
+PKG_VERSION="5de6aa0a1d1a9249a272843e7aba8cdbd601e188"
 PKG_LICENSE="OSS"
-#PKG_SITE="https://github.com/gabomdq/SDL_GameControllerDB"
-PKG_SITE="https://github.com/gabomdq/gamecontrollerdb"
+PKG_SITE="https://github.com/gabomdq/SDL_GameControllerDB"
+#PKG_SITE="https://github.com/gabomdq/gamecontrollerdb"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET=""
 PKG_LONGDESC="A community sourced database of game controller mappings to be used with SDL2 Game Controller functionality"
